1da7c162d8ccc75b1600c970338ca7fb322a2a8b
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2024-03-31 17:15:46 -0300

    :lipstick: create layout

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+    AppColor._();
+    static const Color primaryColor = Color.fromRGBO(177, 170, 195, 1);
+    static const Color secondaryColor = Color.fromRGBO(239, 176, 15, 1);
+    static const Color backgroundColor = Color.fromRGBO(19, 17, 24, 1);
+    static const Color backgroundColorPaper = Color.fromRGBO(24, 21, 30, 1);
+    static const Color textColor = Color.fromRGBO(236, 234, 240, 1);
+}
